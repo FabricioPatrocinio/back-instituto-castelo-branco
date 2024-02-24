4540ea9021e48ed81473d3064b46f05dd0dc10a4
FabricioPatrocinio <fabricioschiffer@gmail.com>
2024-02-24 11:40:06 -0300

    Feature - Melhora logs, trava pynamodb com logs apenas de ERROR

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,4 +1,17 @@
+import logging
+
 from aws_lambda_powertools import Logger
 from settings import settings
 
-logger = Logger(service="nomeDoServico", level=settings.LOG_LEVEL)
+logging.basicConfig()
+
+# Disables all debug logs, this avoids cloudwatch costs
+logging.getLogger("pynamodb").setLevel(logging.CRITICAL)
+logging.getLogger("boto3").setLevel(logging.CRITICAL)
+logging.getLogger("botocore").setLevel(logging.CRITICAL)
+logging.getLogger("nose").setLevel(logging.CRITICAL)
+logging.getLogger("s3transfer").setLevel(logging.CRITICAL)
+logging.getLogger("urllib3").setLevel(logging.CRITICAL)
+
+
+logger = Logger(service="instituto-castelo-branco", level=settings.LOG_LEVEL)
